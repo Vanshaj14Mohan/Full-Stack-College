3a8f5d465cb5704f08cd0817397ac277b5927f6b
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2024-09-18 09:55:01 +0530

    files

--- Node/math.js
+++ Node/math.js
@@ -1,18 +1,30 @@
-// function addition(a, b){
-//     return a+b;
+// // function addition(a, b){
+// //     return a+b;
+// // }
+
+const { multiplication } = require("./file");
+
+// // module.exports = addition();
+
+
+// // function addition(a, b) {
+// //     return a + b;
+// //   }
+  
+// // module.exports = addition(); // Exports the result of 2 + 3
+
+// function addition(a,b){
+//   return a+b;
 // }
 
-// module.exports = addition();
-
-
-// function addition(a, b) {
-//     return a + b;
-//   }
-  
-// module.exports = addition(); // Exports the result of 2 + 3
+// module.exports = addition;
 
 function addition(a,b){
   return a+b;
 }
 
-module.exports = addition;
+function multilplication(a,b){
+  return a*b;
+}
+
+module.exports = {addition, multiplication};
--- Node/new.js
+++ Node/new.js
@@ -1,2 +1,7 @@
-const  mul = require("./file.js");
-console.log(mul(5,9));
+// const  mul = require("./file.js");
+// console.log(mul(5,9));
+
+const math = require("./math");
+
+console.log(math.addition(5,6));
+console.log(math.multiplication(4,5));
