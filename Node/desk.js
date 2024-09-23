16e9d57a2751e6d65648e02587cc53926af5d916
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2024-09-23 09:48:24 +0530

    node demo

---
+++ Node/desk.js
@@ -0,0 +1,13 @@
+const cars = ["BMW", "Volvo", "Mini"];
+let text = "";
+for (let x of cars){
+    text += x;
+}
+console.log(text);
+
+const num = [1,2,3,4,5];
+let num_txt = "";
+for(let x of num){
+    num_txt += x;
+}
+console.log(num_txt);
